61a8ebeb3b9f3cb27e9093f4ec1584e725504c8c
Sai Vikas, Goli <52754670+vikas3624@users.noreply.github.com>
2023-05-21 23:23:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImage1 = "./images/dice" + randomNumber1 +".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
+
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+
+var randomImage2 = "./images/dice" + randomNumber2 +".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
+
+
+
+var sum = randomNumber1 + randomNumber2;
+
+document.querySelector("h2").innerHTML= sum;
